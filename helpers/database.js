1fd0e6627f6ff5dde4a91dd5c5c65663d00694b5
azkura <azizkoura@hotmail.com>
2019-06-10 16:09:31 +0200

    initial folder

--- helpers/database.js
+++ helpers/database.js
@@ -1,19 +1,15 @@
-// const mysql = require('mysql2')
+const mongodb = require('mongodb')
+const MongoClient = mongodb.MongoClient
 
-// const pool = mysql.createPool({
-//     host:'localhost',
-//     user: 'root',
-//     database: 'node-shop',
-//     password: 'pandemon'
-// })
+const mongoConnect = (callback) => {
+    MongoClient.connect(
+        'mongodb+srv://aziz:iEg4z7CzrbmQLrif@cluster0-x3pto.mongodb.net/test?retryWrites=true&w=majority'
+        )
+        .then(client => {
+            console.log('Connected!')
+            callback(client)
+        })
+        .catch(err => console.log(err))
+}
 
-// module.exports = pool.promise()
-
-const Sequelise = require('sequelize')
-
-const sequelize = new Sequelise('node-shop', 'root', 'pandemon', {
-    dialect: 'mysql',
-    host: 'localhost'
-})
-
-module.exports = sequelize
+module.exports = mongoConnect
