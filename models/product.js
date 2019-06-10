700d77ab92ef1ae41da4f3d81c14231048c1fe74
azkura <azizkoura@hotmail.com>
2019-06-10 16:55:30 +0200

    creating products

--- models/product.js
+++ models/product.js
@@ -8,30 +8,16 @@ class Product {
     this.description = description
   }
 
-  save(){}
-}
-
-const Product = sequelize.define('product', {
-  id: {
-    type: Sequelize.INTEGER,
-    autoIncrement: true,
-    allowNull: false,
-    primaryKey: true
-  },
-  title: Sequelize.STRING,
-  price: {
-    type: Sequelize.DOUBLE,
-    allowNull: false
-  },
-  imageUrl: {
-    type: Sequelize.STRING,
-    allowNull: false
-  },
-  description: {
-    type: Sequelize.STRING,
-    allowNull: false
+  save(){
+    const db = getDb()
+    return db.collection('products')
+    .insertOne(this)
+    .then(result => {
+      console.log(result)
+    })
+    .catch(err => console.log(err))
   }
-});
+}
 
 module.exports = Product;
 
