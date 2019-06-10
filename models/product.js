d8418a8ddae5a67c3f41751e0bcce36930781120
azkura <azizkoura@hotmail.com>
2019-06-10 16:31:28 +0200

    connect

--- models/product.js
+++ models/product.js
@@ -1,6 +1,15 @@
-const Sequelize = require('sequelize');
+const getDb = require('../helpers/database').getDb
 
-const sequelize = require('../helpers/database');
+class Product {
+  constructor(title, imageUrl, price, description) {
+    this.title = title,
+    this.imageUrl = imageUrl,
+    this.price = price,
+    this.description = description
+  }
+
+  save(){}
+}
 
 const Product = sequelize.define('product', {
   id: {
@@ -26,43 +35,3 @@ const Product = sequelize.define('product', {
 
 module.exports = Product;
 
-
-
-//_______________________________________________________________________________________
-
-// const db = require('../helpers/database')
-
-// const Cart = require('./cart');
-
-// module.exports = class Product {
-//   constructor(id, title, imageUrl, description, price) {
-//     this.id = id;
-//     this.title = title;
-//     this.imageUrl = imageUrl;
-//     this.description = description;
-//     this.price = price;
-//   }
-
-//   save() {
-//     return db.execute('INSERT INTO products (title, price, description, imageUrl) VALUES (?,?,?,?)',
-//     [this.title, this.price, this.description, this.imageUrl]
-//     )
- 
-//   }
-
-//   static deleteById(id) {
-    
-
-//   }
-
-//   static fetchAll() {
-//     return db.execute('SELECT * FROM products')
-    
-//   }
-
-//   static findById(id) {
-//     return db.execute('SELECT * FROM products WHERE products.id = ?', [id])
-
-//   }
-
-// }
