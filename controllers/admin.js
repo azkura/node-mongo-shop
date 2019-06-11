5415cdef477d35c805f9a81c3fe6f56721a36f6b
azkura <azizkoura@hotmail.com>
2019-06-11 16:15:42 +0200

    finishing the update product (edit)

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,4 +1,7 @@
+const mongodb = require('mongodb')
 const Product = require('../models/product');
+
+const ObjectId = mongodb.ObjectId
 
 exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
@@ -14,7 +17,8 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
   const product = new Product(title, imageUrl, price, description)
-  product.save()
+  product
+  .save()
     .then(result => {
       // console.log(result);
       console.log('Created Product');
@@ -49,22 +53,23 @@ exports.getEditProduct = (req, res, next) => {
 exports.postEditProduct = (req, res, next) => {
   const prodId = req.body.productId;
   const updatedTitle = req.body.title;
-  const updatedPrice = req.body.price;
   const updatedImageUrl = req.body.imageUrl;
+  const updatedPrice = req.body.price;
   const updatedDesc = req.body.description;
-  Product.findById(prodId)
-    .then(product => {
-      product.title = updatedTitle;
-      product.price = updatedPrice;
-      product.description = updatedDesc;
-      product.imageUrl = updatedImageUrl;
-      return product.save();
-    })
-    .then(result => {
-      console.log('UPDATED PRODUCT!');
-      res.redirect('/admin/products');
-    })
-    .catch(err => console.log(err));
+  const product = new Product(
+    updatedTitle, 
+    updatedImageUrl, 
+    updatedPrice,  
+    updatedDesc,
+    new ObjectId(prodId)
+    )
+    product
+      .save() 
+      .then(result => {
+        console.log('UPDATED PRODUCT!');
+        res.redirect('/admin/products');
+      })
+      .catch(err => console.log(err));
 };
 
 exports.getProducts = (req, res, next) => {
