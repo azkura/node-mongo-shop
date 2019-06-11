b1e8429410850701d739099be8ebff3ec49312f4
azkura <azizkoura@hotmail.com>
2019-06-11 22:03:05 +0200

    adding addToCart functionnality

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   User.findById('5cffe71272b9cf0bf9857146')
     .then(user => {
-      req.user = user;
+      req.user = new User(user.name, user.email, user.cart, user._id)
       next();
     })
     .catch(err => console.log(err));
