a3f6115783fb039e9501fea3dd9a8de40f5b7ca8
azkura <azizkoura@hotmail.com>
2019-06-12 23:59:30 +0200

    Using Mongobd to store session

--- app.js
+++ app.js
@@ -4,11 +4,18 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose')
 const session = require('express-session')
+const MongoDBStore = require('connect-mongodb-session')(session)
 
 const errorController = require('./controllers/error');
 const User = require('./models/user');
 
+const MONGODB_URI = 'mongodb+srv://aziz:iEg4z7CzrbmQLrif@cluster0-x3pto.mongodb.net/shop'
+
 const app = express();
+const store = new MongoDBStore({
+  uri: MONGODB_URI,
+  collection: 'sessions'
+})
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -20,7 +27,12 @@ const authRoutes = require('./routes/auth');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(
-  session({secret: 'my secret', resave: false, saveUninitialized: false })
+  session({
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false, 
+    store: store 
+  })
 )
 
 app.use((req, res, next) => {
@@ -38,9 +50,7 @@ app.use(authRoutes);
 
 app.use(errorController.get404);
 
-mongoose.connect(
-  'mongodb+srv://aziz:iEg4z7CzrbmQLrif@cluster0-x3pto.mongodb.net/shop?retryWrites=true&w=majority'
-)
+mongoose.connect( MONGODB_URI )
 .then(result => {
   User.findOne().then(user => {
     if(!user) {
