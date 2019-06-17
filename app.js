ccfe6b2c9e84154faa57ffe9219102b2ea4c14b6
azkura <azizkoura@hotmail.com>
2019-06-17 02:28:53 +0200

    preparing env variables for production

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const errorController = require('./controllers/error');
 const User = require('./models/user');
 
 const MONGODB_URI =
-  'mongodb+srv://aziz:iEg4z7CzrbmQLrif@cluster0-x3pto.mongodb.net/shop';
+  `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@cluster0-x3pto.mongodb.net/${process.env.MONGO_DEFAULT_DATABASE}`
 
 const app = express();
 const store = new MongoDBStore({
@@ -111,7 +111,7 @@ app.use((error, req, res, next) => {
 mongoose
   .connect(MONGODB_URI)
   .then(result => {
-    app.listen(3000);
+    app.listen(process.env.PORT || 3000);
   })
   .catch(err => {
     console.log(err);
